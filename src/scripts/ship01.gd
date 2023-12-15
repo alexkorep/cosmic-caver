cb35f6a8d3e6052d3ea0bf3c224770411fcb9e3a
Alexey Korepanov <alexkorep@gmail.com>
2023-12-15 23:01:20 +0100

    Limit max speed

--- src/scripts/ship01.gd
+++ src/scripts/ship01.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 
-var speed = 0
+export var max_speed = 100
+
+var speed = 0.0
 var rotation_speed = 1.0
-var acceleration = 50
-var deceleration = 50
+var acceleration = 50.0
+var deceleration = 50.0
 
 onready var Camera = $Camera2D
 
@@ -13,13 +15,18 @@ func _ready():
 func _process(delta):
 	if Input.is_action_pressed('ui_up'):
 		speed += acceleration * delta
+		if speed > max_speed:
+			speed = max_speed
 	elif Input.is_action_pressed('ui_down'):
 		speed -= deceleration * delta
+		if speed < -max_speed:
+			speed = -max_speed
 	if Input.is_action_pressed('ui_left'):
 		rotation_degrees -= rotation_speed
 	elif Input.is_action_pressed('ui_right'):
 		rotation_degrees += rotation_speed
 
-	move_and_slide(Vector2.UP.rotated(rotation) * speed)
+	var linear_velocity = Vector2.UP.rotated(rotation) * speed
+	move_and_slide(linear_velocity)
 
 
